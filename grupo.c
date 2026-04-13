3919beba069f4a7e96678fe803db651f8d7b1133
DEMONDAV <david.moscoso@udla.edu.ec>
2026-04-12 20:09:11 -0500

    Parte 1: Definición de matrices y contadores de aprobación

---
+++ grupo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    // 1. Definición de constantes y variables
+    // Según el requerimiento: 5 estudiantes y 3 asignaturas [cite: 18]
+    int estudiantes = 5;
+    int asignaturas = 3;
+    float notas[5][3]; // Matriz para almacenar las notas [cite: 13]
+    
+    // Variables para cálculos
+    float sumaEstudiante, sumaAsignatura;
+    float promedioEstudiante[5];
+    float promedioAsignatura[3];
+    int aprobados[3] = {0, 0, 0};
+    int reprobados[3] = {0, 0, 0};
+}
